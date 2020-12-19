d1839cec9a5c81f0e8a749ae965209fb9fcbf4c3
lauracahe <alu0101129945@ull.edu.es>
2020-12-19 20:51:26 +0000

    Triggers por separado

--- ProyectoFinal/trigger_bonificacion.sql
+++ ProyectoFinal/trigger_bonificacion.sql
@@ -1,36 +1,30 @@
 DELIMITER $$
 USE `SUPERMERCADO`$$
-CREATE DEFINER = CURRENT_USER TRIGGER `SUPERMERCADO`.`compra_bonificacion_BEFORE_INSERT` BEFORE INSERT ON `compra_has_producto` FOR EACH ROW
+CREATE DEFINER = CURRENT_USER TRIGGER `SUPERMERCADO`.`compra_bonificacion_AFTER_INSERT` AFTER INSERT ON `compra_has_producto` FOR EACH ROW
 BEGIN
-	select factura into @factura from compra where factura = new.compra_factura;
-    select cliente_socio_codigo_cliente into @cliente from compra where @factura = new.compra_factura;
+  DECLARE i INT DEFAULT 0;
+  select factura into @factura from compra where factura = new.compra_factura;
+  select cliente_socio_codigo_cliente into @cliente from compra where factura = new.compra_factura;
+  set @total = 0;
+  if @cliente is not NULL then
+    select bonificación into @bonificacion from cliente_socio where codigo_cliente = @cliente;
 
-	create table temporal (compra_factura VARCHAR(45), producto_id_producto INT, cantidad_producto INT);
-    insert into temporal select * from compra_has_producto where compra_factura = @factura;
-
-    select count(compra_factura) into @numero_productos from temporal where compra_factura = @factura;
-    select bonificación into @bonificacion from cliente_socio natural join compra where codigo_cliente = @cliente;
-
-		set @COUNTER = 0;
-        set @total = 0;
-		while @COUNTER < @numero_productos do
-			select producto_id_producto into @id_producto from compra_has_producto where compra_factura = @factura limit 1;
-            select precio into @precio from producto where id_producto = @id_producto;
-            select cantidad_producto into @cantidad from temporal where compra_factura = @factura limit 1;
-
-            set @total = @total + (@cantidad * @precio);
-
-            delete from temporal limit 1;
-            set @COUNTER = @COUNTER + 1;
-        end while;
+    select count(compra_factura) into @n from compra_has_producto where compra_factura = @factura;
+    set i = 0;
+    while i < @n DO
+      select producto_id_producto into @id_producto from compra_has_producto where compra_factura = @factura limit i,1;
+      select precio into @precio from producto where id_producto = @id_producto;
+      select cantidad_producto into @cantidad from compra_has_producto where compra_factura = @factura limit i,1;
+      set @total := @total + (@cantidad * @precio);
+      set i := i + 1;
+    end while;
 
     if @bonificacion > 1.0 then
-		SET @total_con_descuento = @total / @bonificacion;
+      SET @total_con_descuento = @total / @bonificacion;
     end if;
-
     if @total > 100 then
-		set @bonificacion = @bonificacion + 0.01;
+      set @bonificacion = @bonificacion + 0.01;
     end if;
-
-    drop table temporal;
+    UPDATE cliente_socio set bonificación = @bonificacion where codigo_cliente = @cliente;
+  end if;
 END$$
--- ProyectoFinal/trigger_stock.sql
+++ ProyectoFinal/trigger_stock.sql
@@ -1,17 +1,16 @@
 DELIMITER $$
 USE `SUPERMERCADO`$$
-
 CREATE DEFINER = CURRENT_USER TRIGGER `SUPERMERCADO`.`stock_BEFORE_INSERT` BEFORE INSERT ON `compra_has_producto` FOR EACH ROW
 BEGIN
   SET @producto = NEW.producto_id_producto;
   SET @cantidad_producto = NEW.cantidad_producto;
-  SELECT stock_almacen INTO @stock_almacen FROM supermercado_has_producto WHERE producto_id_producto = @producto;
-  SELECT stock_general INTO @stock_general FROM producto WHERE id_producto = @producto;
-  IF @stock_almacen > @cantidad_producto THEN
-    UPDATE producto SET stock_general = @stock_general - @cantidad_producto WHERE id_producto = @producto;
-    UPDATE supermercado_has_producto SET stock_almacen = @stock_almacen - @cantidad_producto WHERE producto_id_producto = @producto;
+
+  select supermercado_nombre into @supermercado from compra where factura = new.compra_factura;
+
+  SELECT stock_almacen INTO @stock_almacen FROM supermercado_has_producto WHERE producto_id_producto = @producto AND supermercado_nombre = @supermercado;
+  IF @stock_almacen >= @cantidad_producto THEN
+    UPDATE supermercado_has_producto SET stock_almacen = @stock_almacen - @cantidad_producto WHERE producto_id_producto = @producto AND supermercado_nombre = @supermercado;
   ELSE
     SIGNAL SQLSTATE 'ERR0R' SET MESSAGE_TEXT = 'No hay stock suficiente del producto';
   END IF;
 END$$
-
